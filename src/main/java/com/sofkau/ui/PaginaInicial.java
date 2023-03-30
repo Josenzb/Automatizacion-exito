ffca8a78a949df944508fcbd67595633d81312db
nunezjose770 <nunezjose770@gmail.com>
2023-03-30 12:09:01 -0500

    Se configura el file properties y el inicio de sesion

--- src/main/java/com/sofkau/ui/PaginaInicial.java
+++ src/main/java/com/sofkau/ui/PaginaInicial.java
@@ -8,4 +8,17 @@ public class PaginaInicial extends PageObject {
 
     public static final Target BOTON_CUENTA = Target.the("Boton mi cuenta")
             .located(By.cssSelector(".vtex-menu-2-x-styledLinkIcon.vtex-menu-2-x-styledLinkIcon--header-link.mh2"));
+
+    public static final Target BOTON_INICIO_USER_PASS = Target.the("Boton inicar con usuario y contrasena")
+            .located(By.xpath("//div[contains(@class,'vtex-login-2-x-button vtex-login-2-x-emailPasswordOptionBtn')]//button[contains(@type,'button')]"));
+
+    public static final Target CAMPO_USUARIO = Target.the("Campo escribir usuario")
+            .located(By.xpath("//input[contains(@placeholder,'Ingresa tu email')]"));
+
+    public static final Target CAMPO_CONTRASENNA = Target.the("Campo escribir usuario")
+            .located(By.xpath("//input[@placeholder='Ingresa tu contraseña']"));
+
+    public static final Target BOTON_INICIAR_SESION = Target.the("Boton mi cuenta")
+            .located(By.cssSelector(".vtex-button.bw1.ba.fw5.v-mid.relative.pa0.lh-solid.br2.min-h-small.t-action--small.bg-action-primary.b--action-primary.c-on-action-primary.hover-bg-action-primary.hover-b--action-primary.hover-c-on-action-primary.pointer"));
+
 }
--- src/test/java/com/sofkau/stepdefinitions/InicioSesionStepDefinitions.java
+++ src/test/java/com/sofkau/stepdefinitions/InicioSesionStepDefinitions.java
@@ -6,10 +6,19 @@ import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 
+import java.io.IOException;
+import java.util.List;
+
 import static com.sofkau.tasks.IniciarSesion.iniciarSesion;
+import static com.sofkau.util.SetVeriables.getUserPasword;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class InicioSesionStepDefinitions extends Configuracion {
+
+    private List<String> credenciales = getUserPasword();
+
+    public InicioSesionStepDefinitions() throws IOException {
+    }
 
     @Dado("que el usuario esta en la pagina de inicio")
     public void queElUsuarioEstaEnLaPaginaDeInicio() {
@@ -21,7 +30,10 @@ public class InicioSesionStepDefinitions extends Configuracion {
 
     @Cuando("completa los campos para iniciar sesion")
     public void completaLosCamposParaIniciarSesion() {
-        iniciarSesion();
+        theActorInTheSpotlight().wasAbleTo(
+                iniciarSesion().conElUsuario(credenciales.get(0))
+                        .yConLaContrasenna(credenciales.get(1))
+        );
     }
 
     @Entonces("el usuario iniciara sesion y vera un mensaje de bienvenida")
